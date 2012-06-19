8ad6994fbf1469e3626ec3557b2da94ec4d59a1f
Harshal <harshal2288shinde@gmail.com>
2012-06-19 19:00:23 +0530

    data saved

--- index.php
+++ index.php
@@ -3,60 +3,95 @@
         Security checks
      </head>
     <body>
-<form method="post" onsubmit="result.php">       
-        <table  border="2">
-        <tr>
-        <td>Enter Usre Name
-        </td>
-        <td>
-        <input type="text" name="unmae" id="name">
-        </td>
-        
-        </tr>
-
-		<tr>
-        <td>Enter password
-        </td>
-        <td>
-        <input type="password" name="upass" id="pass">
-        </td>
-        
-        </tr>
-
-</table>
-</form> 	      
- <input type="submit" id="submit" name="submit" > 
-    </body>
- </html>
+ 	      
 <?php
-$user_name = "root";
-$password = "root";
-$database = "harshal";
-$server = "127.0.0.1";
-$db_handle = mysql_connect($server, $user_name, $password);
+error_reporting(0);
+echo"$token";
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
-$db_found = mysql_select_db($database, $db_handle);
+$username= "'".$_POST['uname']."'";
+$filteredUserName = htmlspecialchars($username);//To avoid xss attack
+$password ="'".$_POST['password']."'";
+$filteredUserPass = htmlspecialchars($password);//to avoid xss attack
 
-if ($db_found) 
+connection();
+Login($filteredUserName,$filteredUserPass);
+
+//SQL($filteredUserName,$filteredUserPass);
+
+
+
+
+function Login($filteredUserName,$filteredUserPass)
 {
-$SQL = "SELECT * FROM admin";
-$result = mysql_query($SQL);
-//print "Database Found ";
+    if(empty($filteredUserName))
+    {
+        $this->HandleError("UserName is empty!");
+        return false;
+    }
+    if(empty($filteredUserPass))
+    {
+        $this->HandleError("Password is empty!");
+        return false;
+    }
+    
+    
+    if(!SQL($filteredUserName,$filteredUserPass))
+    {
+        return false;
+    }
+    session_start();
+    $_SESSION[$this->GetLoginSessionVar()] = $username;
+    return true;
+}
+
+
+
+
+function SQL($username,$password) 
+{	
+echo"I am in sql";
+	//echo"$username";
+	//echo"$password";
+		//mysql_real_escape_string($username);
+		//mysql_real_escape_string($password);
+			$query = mysql_query("SELECT uname,password FROM admin WHERE uname= $username  and password= $password ;");
+			echo "sql returns $query"."<br />";
+			$result = mysql_query($query);
 while ($db_field = mysql_fetch_assoc($result))
 {
-print $db_field['id'] . "<BR>";
+//print $db_field['id'] . "<BR>";
 print $db_field['uname'] . "<BR>";
 print $db_field['password'] . "<BR>";
 
 }
-
-mysql_close($db_handle);
-}
-else {
-print "Database NOT Found ";
 }
 
 
+function connection()
+	{
+	$user_name = "root";
+	$password = "root";
+	$database = "harshal";
+	$server = "127.0.0.1";
+	$db_handle = mysql_connect($server, $user_name, $password);
+	$db_found = mysql_select_db($database, $db_handle);
+	if ($db_found) 
+	{
+	
 
+		print "Database Found ";
+		mysql_close($db_handle);
+		}
+		else 
+		{
+			print "Database NOT Found ";
+		}
 
+}//end of connection
 
+?>
+
+   </body>
+ </html>
